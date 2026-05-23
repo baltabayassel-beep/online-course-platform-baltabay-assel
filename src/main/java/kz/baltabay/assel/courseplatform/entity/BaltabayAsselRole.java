163f7d59ae985e7974e16badd8d88d02b4218bdd
Baltabay Assel <assel.baltabay@example.com>
2026-05-23 10:03:08 +0500

    Model users and roles

---
+++ src/main/java/kz/baltabay/assel/courseplatform/entity/BaltabayAsselRole.java
@@ -0,0 +1,7 @@
+package kz.baltabay.assel.courseplatform.entity;
+
+public enum BaltabayAsselRole {
+    ROLE_ADMIN,
+    ROLE_INSTRUCTOR,
+    ROLE_STUDENT
+}
---
+++ src/main/java/kz/baltabay/assel/courseplatform/entity/BaltabayAsselUser.java
@@ -0,0 +1,43 @@
+package kz.baltabay.assel.courseplatform.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class BaltabayAsselUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<BaltabayAsselRole> roles = new HashSet<>();
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
